ca8f39d83b95dd9a94be8520152cb0a2089a296c
Frances Cortuna <francescortuna@gmail.com>
2022-11-05 02:08:01 -0400

    Added view roles feature

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
-const inquirer = require('inquirer');
-const viewDepartments = require('./lib/viewDepartments');
+const inquirer = require("inquirer");
+const View = require("./lib/query");
 
 // Menu
 const menu = () => {
-    inquirer
-        .prompt([
-            {
-                type: 'list',
-                name: 'request',
-                message: 'What would you like to do?',
-                choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
-            }
-        ])
-        .then(({ request }) => { // Deconstructing user request
-            switch(request) {
-                case 'View all departments':
-                    viewDepartments();
-                    break;
-                default:
-                    console.log('Please choose from list.');
-                    menu();
-            }
-        })
-}
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "request",
+        message: "What would you like to do?",
+        choices: [
+            "View all departments",
+            "View all roles",
+            "View all employees",
+            "Add a department",
+            "Add a role",
+            "Add an employee",
+            "Update an employee role",
+        ],
+      },
+    ])
+    .then(({ request }) => {
+      // Deconstructing user request
+      switch (request) {
+        case "View all departments":
+            const viewDepQueryReq = "SELECT * FROM department";
+            const viewDepQuery = new View(viewDepQueryReq);
+            viewDepQuery.logQuery();
+            break;
+        case "View all roles":
+            const viewRoleQueryReq = `SELECT role.id, role.title, role.salary, department.name 'department' 
+                    FROM role
+                        JOIN department ON role.department_id = department.id`;
+            const viewRoleQuery = new View(viewRoleQueryReq);
+            viewRoleQuery.logQuery();
+            break;
+        default:
+            console.log("Please choose from list.");
+            menu();
+      }
+    });
+};
 
 menu();
